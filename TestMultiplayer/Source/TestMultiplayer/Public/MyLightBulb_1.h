b07477d94a02b0ad4f700c83ee4956e847b92e21
vkddnrs <vkddnrs@gmail.com>
2021-02-26 22:38:09 +0300

    Сделаны начальные установки в проекте и созданы классы для плиты и лампочек.

---
+++ TestMultiplayer/Source/TestMultiplayer/Public/MyLightBulb_1.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LightBulb.h"
+#include "MyLightBulb_1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTMULTIPLAYER_API AMyLightBulb_1 : public ALightBulb
+{
+	GENERATED_BODY()
+	
+};
